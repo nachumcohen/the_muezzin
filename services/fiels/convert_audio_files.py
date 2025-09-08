7d6b50018259b518952d73bfa3244e51ed1962a0
nachum <nahum0583@gmail.com>
2025-09-08 17:50:00 +0300

    add function convert audio to text

---
+++ services/fiels/convert_audio_files.py
@@ -0,0 +1,13 @@
+import whisper
+from logger import Logger
+
+logger = Logger.get_logger()
+def convert_audio_to_text(file):
+
+    try:
+        model = whisper.load_model("base")
+        result = model.transcribe(file)
+        return result["text"]
+
+    except Exception as e:
+        logger.error(e)
