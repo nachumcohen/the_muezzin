361b2f280df2be6b968debbfc65056fb39bafe00
nachum <nahum0583@gmail.com>
2025-09-08 09:12:41 +0300

    routing and naming changes

--- services/file_path.py
+++ services/file_path.py
@@ -1,5 +1,5 @@
+import json
 import os
-from datetime import datetime
 from pathlib import Path
 
 def get_files_path_in_folder(folder_path):
@@ -13,13 +13,18 @@ def get_files_path_in_folder(folder_path):
 
 def metadata_by_file_path(file_path):
 
-    metadata = {}
+    metadata_dict = {}
     file_path = Path(file_path)
     if file_path.exists():
         file_stats = file_path.stat()
-        metadata["name"] = file_path.name
-        metadata["size"] = file_stats.st_size
-        metadata["creation_date"] = datetime.fromtimestamp(file_stats.st_ctime)
-        metadata["last_modified_date"] = datetime.fromtimestamp(file_stats.st_mtime)
+        metadata_dict["name"] = file_path.name
+        metadata_dict["size"] = file_stats.st_size
+        metadata_dict["creation_date"] = file_stats.st_ctime
+        metadata_dict["last_modified_date"] = file_stats.st_mtime
 
-    return metadata
+    return metadata_dict
+
+
+def convert_dict_to_json(dict_to_convert : dict):
+    dict_to_json = json.dumps(dict_to_convert)
+    return dict_to_json
---
+++ services/kafka/consumer.py
@@ -0,0 +1,14 @@
+from connections.kafka_connection import create_consumer
+
+class Consumer:
+    def __init__(self , topic_name:str , group_id:str=None, auto_offset_reset="earliest"):
+        self.consumer = create_consumer(topic_name , group_id , auto_offset_reset)
+
+
+    def stop_consumers(self):
+        self.consumer.close()
+
+    def run_consumer(self):
+        print("Starting consumer...")
+        for message in self.consumer:
+            yield message.value
